2fadefee3f8e65adb9cf6dfc7a61293af7f24d6f
cpmachado <cpmachado@protonmail.com>
2025-04-25 02:16:43 +0100

    MVP

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
+import polars as pl
+import argparse
+
 
 def main():
-    print("Hello from midfide!")
+    parser = argparse.ArgumentParser(
+        usage="A simple program to see how mid you are in FIDE"
+    )
+    parser.add_argument("id", type=int, help="FIDE id")
+    args = parser.parse_args()
+
+    df = pl.read_csv("data/players.csv", has_header=True, infer_schema_length=1000)
+    cols = ["rating", "rapid_rating", "blitz_rating"]
+    player = df.filter(pl.col("id") == args.id).select(
+        pl.col("id"), pl.col("name"), *map(pl.col, cols)
+    )
+    print(player)
+
+    p = [(col, pl.col(col), player.select(col).item()) for col in cols]
+
+    print(
+        pl.DataFrame(
+            dict(
+                (f"percentile({name})", df.filter(x > 0).select(x <=
+                                               v).cast(pl.Int8).mean().item() *
+                 100)
+                for name, x, v in p
+            )
+        )
+    )
 
 
 if __name__ == "__main__":
